8519e826f1989be5da657ad5c82fd6de0528f328
kounch <kounch@users.noreply.github.com>
2022-09-05 20:27:18 +0200

    CMake PDF gen and MacOS Builds

---
+++ CMake/Find/Findasciidoctor-pdf.cmake
@@ -0,0 +1,18 @@
+# Copyright (c) 2022 kounch
+# SPDX-License-Identifier: BSD-2-Clause
+
+find_program(ASCIIDOCTOR_PDF_BINARY NAMES asciidoctor-pdf
+	HINTS ${ASCIIDOCTOR_PDF_BINARY_PATH})
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(asciidoctor-pdf DEFAULT_MSG
+                                  ASCIIDOCTOR_PDF_BINARY)
+
+mark_as_advanced(ASCIIDOCTOR_PDF_BINARY)
+
+set(ASCIIDOCTOR_PDF_BINARIES ${ASCIIDOCTOR_PDF_BINARY})
+
+if(NOT ASCIIDOCTOR-PDF_FOUND)
+	MESSAGE(FATAL_ERROR "asciidoctor-pdf not found")
+endif()
---
+++ CMake/Find/Findasciidoctor.cmake
@@ -0,0 +1,18 @@
+# Copyright (c) 2022 kounch
+# SPDX-License-Identifier: BSD-2-Clause
+
+find_program(ASCIIDOCTOR_BINARY NAMES asciidoctor
+	HINTS ${ASCIIDOCTOR_BINARY_PATH})
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(asciidoctor DEFAULT_MSG
+                                  ASCIIDOCTOR_BINARY)
+
+mark_as_advanced(ASCIIDOCTOR_BINARY)
+
+set(ASCIIDOCTOR_BINARIES ${ASCIIDOCTOR_BINARY})
+
+if(NOT ASCIIDOCTOR_FOUND)
+	MESSAGE(FATAL_ERROR "asciidoctor not found")
+endif()
